da4155293b77dfba9674439f8db0f70858324b9d
Lucas Horta <lucashortamoreira6@gmail.com>
2022-10-27 17:52:16 -0300

    Atividade 2 - Exercicio 2

---
+++ Atividade 2 - Exercicio 2.py
@@ -0,0 +1,9 @@
+# Digete o numero
+num = float(input("Digite um numero: "))
+
+if num > 10:
+    print("O número digitado é maior que 10")
+elif num < 10:
+    print("O número digitado é menor que 10")
+else :
+    print("O número digitado é igual a 10")
